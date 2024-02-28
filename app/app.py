5223f995c320431b0f3f9944d4cae62b51ddba0a
DiegoM <diegomesaglwh@gmail.com>
2024-02-28 09:33:42 -0500

    BASE DE DATOS FLASK

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
-from flask import *
-
+from flask import   *     #Flask,render_template
+import mysql.connector
 #Creamos una instancia de la clase Flask
 
 app = Flask(__name__)
 
-#Ejecutar la apliación uwu
+#CONFIGURAR LA CONEXIÓN
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="agenda2024"
 
+
+)
+cursor = db.cursor()
 
 #Definir rutas
 
@@ -14,9 +22,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/Registrar')
-def registrar():
-    return render_template('Registrar.html')
+@app.route('/Registrar' , methods=['POST'] )
+def registrar_usuario():
+    Nombres = request.form['nombre'],
+    Apellidos = request.form['apellido'],
+    Email = request.form['correo'],
+    Direccion = request.form['direccion'],
+    Telefono = request.form['numero'],
+    Usuario = request.form['usuario'],
+    Contrasena = request.form['contrasena']
+
+    #insertar datos a la tabla personas
+
+    cursor.execute("INSERT IN TO personas(nombreper,apellidoper,emailper,dieccionper,telefonoper,usuarioper,contraseña)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombres,Apellidos,Email,Direccion,Telefono,Usuario,Contrasena))
+    db.commit()
+
+
+    
+    return redirect(url_for('/Registrar'))
 
 if __name__ == '__main__':
     app.add_url_rule('/',view_func=index)
