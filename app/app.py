f324df1943cd57056e26113cff0f6c874a85bcc6
DiegoM <diegomesaglwh@gmail.com>
2024-03-04 09:41:29 -0500

    FLASK3

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template,redirect,url_for, request
+from flask import Flask,render_template,redirect,url_for,flash,request
 import mysql.connector
 #Creamos una instancia de la clase Flask
 
 app = Flask(__name__)
-
+app.secret_key = 'your_secret_key'
 #CONFIGURAR LA CONEXIÓN
 db = mysql.connector.connect(
     host="localhost",
@@ -18,8 +18,12 @@ cursor = db.cursor()
 #Definir rutas
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def lista():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM personas')
+    usuario =  cursor.fetchall()
+
+    return render_template('index.html', personas=usuario)
 
 
 @app.route('/Registrar' , methods=['GET','POST'] )
@@ -37,6 +41,8 @@ def registrar_usuario():
 
         cursor.execute("INSERT INTO personas(nombreper,apellidoper,emailper,direccionper,telefonoper,usuarioper,contraseña) VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombres,Apellidos,Email,Direccion,Telefono,Usuario,Contrasena))
         db.commit()
+        flash('Usuario creado correctamente','success')
+
 
 
     #En el caso de que sea una solicitud, redirige a la misma pagina
@@ -47,5 +53,5 @@ def registrar_usuario():
     return render_template('Registrar.html')
 
 if __name__ == '__main__':
-    app.add_url_rule('/',view_func=index)
+    app.add_url_rule('/',view_func=lista)
     app.run(debug=True,port=5005)
