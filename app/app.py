16ad6cd33f7a1894281ed76e60a4d3a38de4302a
DiegoM <diegomesaglwh@gmail.com>
2024-03-05 09:50:06 -0500

    FLASK

--- app/app.py
+++ app/app.py
@@ -52,6 +52,42 @@ def registrar_usuario():
     #Método get, renderiza el formulario
     return render_template('Registrar.html')
 
+@app.route('/Editar/<int:id>' ,methods = ['GET','POST'])
+def editar_usuario(id):
+    cursor = db.cursor()
+    if request.method == 'POST':
+        nombrepersona = request.form.get('nombrepersona')
+        apellidopersona = request.form.get('apellidopersona')
+        emailpersona = request.form.get('emailpersona')
+        direccionpersona = request.form.get('direccionpersona')
+        telefonopersona = request.form.get('telefonopersona')
+        usuariopersona = request.form.get('usuariopersona')
+        contraseñapersona = request.form.get('contraseñapersona')
+
+    #sentencia para actualizar los datos
+        sql = "UPDATE personas set nombreper=%s,apellidoper=%s,emailper=%s,direccionper=%s,telefonoper=%s,usuarioper=%s,contraseña=%s where polper=%s"
+        cursor.execute(sql,(nombrepersona,apellidopersona,emailpersona,direccionpersona,telefonopersona,usuariopersona,contraseñapersona,id))
+        db.commit()
+
+        return redirect(url_for('lista'))
+    else:
+        #obtener los datos de la persona que va a editar
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM personas WHERE polper = %s', (id,))
+        data = cursor.fetchall()
+
+        return render_template('Editar.html' , personas=data[0])
+
+@app.route('/eliminar/<int:id>', methods=['GET'])
+def eliminar_usuario(id):
+    
+    return redirect(url_for('lista'))
+
+
+
+
+
+
 if __name__ == '__main__':
     app.add_url_rule('/',view_func=lista)
     app.run(debug=True,port=5005)
